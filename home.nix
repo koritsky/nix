31c25529fc619f7a2ed296389820eef16df210b3
koritsky <koritcky@gmail.com>
2026-05-11 12:33:56 +0200

    rm stylix autoEnable

--- home.nix
+++ home.nix
@@ -32,13 +32,22 @@ in
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
-    autoEnable = true;
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font";
       };
       sizes.terminal = 14;
+    };
+    autoEnable = false;
+    targets = {
+      font-packages.enable = true;
+      fontconfig.enable = true;
+      bat.enable = true;
+      fzf.enable = true;
+      starship.enable = true;
+      zellij.enable = true;
+      helix.enable = true;
     };
   };
 
